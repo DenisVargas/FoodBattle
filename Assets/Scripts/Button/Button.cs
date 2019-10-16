8698b6a02c38d36e067c23b58f659034ecf52ecf
nikoTanya <55672977+nikoTanya@users.noreply.github.com>
2019-10-16 04:25:20 -0300

    win-loser

---
+++ Assets/Scripts/Button/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void tryAgain(string TryAgain)
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void exitApplication (string Exit)
+    {
+        Application.Quit();
+    }
+}
